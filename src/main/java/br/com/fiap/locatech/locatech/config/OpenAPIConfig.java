5d8f2f87b0368ab5932d9023e86afdf2a5314eca
Gustavo Leite <gustavo.jog@hotmail.com>
2025-04-27 19:12:33 -0300

    feat: add openapi swagger documentation

---
+++ src/main/java/br/com/fiap/locatech/locatech/config/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package br.com.fiap.locatech.locatech.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI localTech() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Loca Tech API")
+                                .description("Projeto desenvolvido durante o curso Spring MVC")
+                                .version("v0.0.1")
+                                .license(new License().name("Apache 2.0").url("https://github.com/gustavoleite"))
+                );
+    }
+}
--- src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
@@ -2,6 +2,9 @@ package br.com.fiap.locatech.locatech.controllers;
 
 import br.com.fiap.locatech.locatech.entities.Veiculo;
 import br.com.fiap.locatech.locatech.services.VeiculoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/veiculos")
+@Tag(name = "Veículo", description = "Controle para CRUD de veículos")
 public class VeiculoController {
 
     private static final Logger logger = LoggerFactory.getLogger(VeiculoController.class);
@@ -24,6 +28,13 @@ public class VeiculoController {
     }
 
     // http://localhost:8080/veiculos?page=1&size=10
+    @Operation(
+            description = "Busca todos os veículos paginas",
+            summary = "Busca de veículos",
+            responses = {
+                    @ApiResponse(description = "Ok", responseCode = "200")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<Veiculo>> findAllVeiculos(
             @RequestParam("page") int page,
